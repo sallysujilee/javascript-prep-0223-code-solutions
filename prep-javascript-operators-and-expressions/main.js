1d7fe2f603eb98a9b773e66c5f77be54b76c44b5
Sally Lee <sallysujilee@gmail.com>
2023-03-05 23:00:16 -0800

    Completed prep-javascript-operators-and-expressions exercise.

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+const number1 = 2;
+const number2 = 3;
+const product = number1 * number2;
+console.log('value of product:', product)
+console.log('type of product:', product);
+
+const charge = 1;
+const payment = 5;
+const amountRemaining = payment - charge;
+console.log('value of product:', amountRemaining);
+console.log('type of product:', amountRemaining);
+
+const tests = 60;
+const assignments = 100;
+const final = 80;
+const grade = (tests * assignments * final)/3;
+console.log('value of product:', grade);
+console.log('type of product:', grade);
+
+const firstName = "Sally";
+const lastName = "Lee";
+const fullName = firstName + lastName;
+console.log('value of product:', fullName);
+console.log('type of product:', fullName);
+
+const pH = 5;
+const isAcidic = pH < 7;
+console.log('value of product:', pH);
+console.log('type of product:', isAcidic);
+
+const numberOfSoldiers = 300;
+const isSparta = numberOfSoldiers === 300;
+console.log('value of product:', numberOfSoldiers);
+console.log('type of product:', isSparta);
+
+let nickname = fullName;
+nickname += " is the GOAT";
+console.log('value of product:', nickname);
+console.log('type of product:', nickname);
