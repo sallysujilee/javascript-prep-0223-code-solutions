3af7850306cd73c7a74815ab925fb258f2ecec5e
Sally Lee <sallysujilee@gmail.com>
2023-03-05 23:42:45 -0800

    Completed prep-javascript-operators-and-expressions exercise.

--- prep-javascript-operators-and-expressions/main.js
+++ prep-javascript-operators-and-expressions/main.js
@@ -7,33 +7,33 @@ console.log('type of product:', product);
 const charge = 1;
 const payment = 5;
 const amountRemaining = payment - charge;
-console.log('value of product:', amountRemaining);
-console.log('type of product:', amountRemaining);
+console.log('value of amountRemaining:', amountRemaining);
+console.log('type of amountRemaining:', amountRemaining);
 
 const tests = 60;
 const assignments = 100;
 const final = 80;
 const grade = (tests * assignments * final)/3;
-console.log('value of product:', grade);
-console.log('type of product:', grade);
+console.log('value of grade:', grade);
+console.log('type of grade:', grade);
 
 const firstName = "Sally";
 const lastName = "Lee";
 const fullName = firstName + lastName;
-console.log('value of product:', fullName);
-console.log('type of product:', fullName);
+console.log('value of fullName:', fullName);
+console.log('type of fullName:', fullName);
 
 const pH = 5;
 const isAcidic = pH < 7;
-console.log('value of product:', pH);
-console.log('type of product:', isAcidic);
+console.log('value of isAcidic:', pH);
+console.log('type of isAcidic:', isAcidic);
 
 const numberOfSoldiers = 300;
 const isSparta = numberOfSoldiers === 300;
-console.log('value of product:', numberOfSoldiers);
-console.log('type of product:', isSparta);
+console.log('value of isSparta:', numberOfSoldiers);
+console.log('type of isSparta:', isSparta);
 
 let nickname = fullName;
 nickname += " is the GOAT";
-console.log('value of product:', nickname);
-console.log('type of product:', nickname);
+console.log('value of nickname:', nickname);
+console.log('type of nickname:', nickname);
