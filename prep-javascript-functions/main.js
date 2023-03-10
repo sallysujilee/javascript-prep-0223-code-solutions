e12646a232bfae818770089333ed373693322af0
Sally Lee <sallysujilee@gmail.com>
2023-03-10 15:45:58 -0800

    Completed prep-javascript-functions exercise.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,87 @@
+function addTwoNumbers(a, b) {
+  return a + b;
+}
+
+const addTwoNumbersResult = addTwoNumbers (2, 2);
+
+console.log("addTwoNumbers Exercise:", addTwoNumbersResult);
+
+//--------------
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+const convertHoursToMinutesResult = convertHoursToMinutes(2);
+
+console.log("convertHoursToMinutes Exercise:", convertHoursToMinutesResult);
+
+//------------------
+
+function getGreeting(name) {
+  return "Hello" + " " + name;
+}
+
+const getGreetingResult = getGreeting('World');
+
+console.log("getGreeting excerise:", getGreetingResult);
+
+//----------------------
+
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+
+const addAndMultiplyBy5Result = addAndMultiplyBy5(10, 5);
+
+console.log("addAndMultiplyBy5 exercise:", addAndMultiplyBy5Result);
+
+//------------------------
+
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1 * num2)/5;
+}
+
+const multiplyAndDivideBy5Result = multiplyAndDivideBy5(35, 10);
+
+console.log("multiplyAndDivideBy5 exercise:", multiplyAndDivideBy5Result);
+
+//----------------------
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+
+const subtractTwoNumbersResult = subtractTwoNumbers(22,7);
+
+console.log("subtractTwoNumbers exercise:", subtractTwoNumbersResult);
+
+//------------------
+
+function getCircleCircumference(radius) {
+  return 2 * Math.PI * radius;
+}
+
+const getCircleCircumferenceResult = getCircleCircumference(5);
+
+console.log("getCircleCircumference exercise:", getCircleCircumferenceResult);
+
+//-------------------
+
+function getFullName(firstName, lastName) {
+  return firstName + " " + lastName;
+}
+
+const getFullNameResult = getFullName("Sally", "Lee");
+
+console.log("getFullName:", getFullNameResult);
+
+//---------------------
+
+function cube(number) {
+  return number ** 3
+}
+
+const cubeResult = cube(5);
+
+console.log("cubeResult:", cubeResult);
---
+++ prep-javascript-functions/prep-javascript-functions-quiz-notes.md
@@ -0,0 +1,40 @@
+# prep-javascript-functions-quiz-notes
+
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- What are the five parts of a function definition?
+
+- How do you call a function?
+
+- What is the difference between a parameter and an argument?
+
+## Notes
+
+All student notes should be written here.
+1. The five parts of a function definition are: defining, calling, returning, naming, and anonymous functions.
+
+2. To call a function, you write the function's name and then have a comma-separated list of zer oro more arguments surrounded by parentheses.
+
+How to write `Code Examples` in markdown
+
+for JS:
+```javascript
+const data = "Howdy"
+```
+
+for HTML:
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+```css
+div {
+  width:100%
+}
+```
